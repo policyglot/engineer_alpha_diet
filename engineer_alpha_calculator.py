d4afbc36b1ca8f2c51a429c02ab600b33f630da0
Abhishek Pandit <policyglot@users.noreply.github.com>
2020-08-27 22:31:38 -0500

    feat : add basic dictionaries for macros
    These dictionaries will form the basis of the full fledged
    Diet class.
    
    The current goal is to fully understand what the four stages of
    the program have in common for their diet prescriptions.

---
+++ engineer_alpha_calculator.py
@@ -0,0 +1,29 @@
+'''
+This program calculates the required macros of foods to be eaten for the 4 phases of the Engineering the Alpha program
+'''
+
+Class ():
+training non_training
+
+week_no
+
+#Carb Intake on Workout Days
+{1: 0
+2: 30
+3: 75
+4: 100}
+
+
+Maintenance_Caloric_
+
+Intake
+
+#Subtract
+wkt = 300
+non_wkt = 500
+
+protein_lbm:
+wkt = 0.8
+non_wkt = 0.7
+
+cal_per_g = 4
