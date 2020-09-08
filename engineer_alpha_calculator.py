04eb05eddcf41256239f1d89713cff951279a6ac
Abhishek Pandit <policyglot@users.noreply.github.com>
2020-09-08 10:02:47 -0500

    debug: fix calculation error in fatfree function and add test cases
    There is still some mistake in the carb_intake function based on test results for the Adapt phase. The next step would be check each one line by line

--- engineer_alpha_calculator.py
+++ engineer_alpha_calculator.py
@@ -7,11 +7,13 @@ cal_per_g = {'carb': 4,
              'protein': 4,
              'fat': 9}
 
-class alpha_workout():
+
+class Alpha_workout():
     """
     Provides an interface for understanding the overall architecture of the project
     This conducts backend calculations which then connect with the UI classes of the calculator
     """
+
     def __init__(self, stage, week, bodywt, bodyfat):
         self.stage = stage
         self.week = week
@@ -19,7 +21,7 @@ class alpha_workout():
         self.bodyfat = bodyfat
 
     def calculate_fatfree(self):
-        return self.bodywt * (1 - self.bodyfat)
+        return self.bodywt * (100 - self.bodyfat) / 100
 
     def calculate_maintain(self):
         """Use body fat to decide constant with which to multiply fatfree mass
@@ -56,24 +58,24 @@ class alpha_workout():
         :param day: str- workout day ('wkt') or non-workout ('nonwkt')
         :return: the amount of deficit required from resting metabolic rate
         """
-        deficit_dict = {'prime':{'wkt': 300, 'non_wkt': 500},
-                   'adapt': {'wkt': 200, 'non_wkt': 600},
-                   'surge': {'wkt': -400, 'non_wkt': 200}
-                   }
+        deficit_dict = {'prime': {'wkt': 300, 'non_wkt': 500},
+                        'adapt': {'wkt': 200, 'non_wkt': 600},
+                        'surge': {'wkt': -400, 'non_wkt': 200}
+                        }
         return deficit_dict[self.stage][day]
 
     def final_calories(self, day):
         return self.maintenance_calories() - self.deficit(day)
 
-    def carbohydrate_cals(self, prime=False, day):
+    def carb_cals(self, day, prime=False):
         """
         :param prime- Boolean- checks for whether the trainee is in the
         :return:
         """
         carbs_fixed = {1: {'wkt': 0, 'nonwkt': 0},
-                 2: {'wkt': 0, 'nonwkt': 30},
-                 3: {'wkt': 0, 'nonwkt': 50},
-                 4: {'wkt': 0, 'nonwkt': 75}}
+                       2: {'wkt': 0, 'nonwkt': 30},
+                       3: {'wkt': 0, 'nonwkt': 50},
+                       4: {'wkt': 0, 'nonwkt': 75}}
         if prime:
             return carbs_fixed[self.week][day]
         else:
@@ -82,7 +84,7 @@ class alpha_workout():
                         'complete': {'wkt': 1, 'nonwkt': 0.25}
                         }
             carb_cal = carb_lbm[self.stage][day] * self.calculate_fatfree()
-            return carb_cal/cal_per_g['carb']
+            return carb_cal / cal_per_g['carb']
 
     def protein_cals(self, day):
         """
@@ -95,9 +97,9 @@ class alpha_workout():
         protein_lbm = {'adapt': {'wkt': 1, 'non_wkt': 0.8},
                        'prime': {'wkt': 0.8, 'non_wkt': 0.7},
                        'surge': {'wkt': 1.5, 'non_wkt': 1.25},
-                       'complete': {'wkt':1.5, 'nonwkt': 1}
-                   }
-        protein_cal =  protein_lbm[self.stage][day] * self.calculate_fatfree()\
+                       'complete': {'wkt': 1.5, 'non_wkt': 1}
+                       }
+        protein_cal = protein_lbm[self.stage][day] * self.calculate_fatfree()
         return protein_cal
 
     def carb_intake(self, day):
@@ -111,3 +113,14 @@ class alpha_workout():
 
     def fat_intake(self, day):
         return self.fat_cals(day) / cal_per_g['fat']
+
+
+test_case: Alpha_workout = Alpha_workout('adapt', 1, 166, 13)
+
+print(test_case.calculate_fatfree())
+print(test_case.calculate_maintain())
+print(test_case.maintenance_calories())
+print(test_case.deficit('wkt'))
+
+print(test_case.protein_intake('wkt'))
+print(test_case.carb_intake('wkt'))
